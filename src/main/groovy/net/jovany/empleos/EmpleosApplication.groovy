7a69a68264cd68f50bf0cc7f7973ab4b46472c70
Jovany Vergara <jovany@makingdevs.com>
2021-10-20 14:34:18 -0500

    agregando heroku

--- src/main/groovy/net/jovany/empleos/EmpleosApplication.groovy
+++ src/main/groovy/net/jovany/empleos/EmpleosApplication.groovy
@@ -2,9 +2,18 @@ package net.jovany.empleos
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 class EmpleosApplication {
+
+	@RequestMapping("/home")
+	@ResponseBody
+	String home() {
+		return "home";
+	}
 
 	static void main(String[] args) {
 		SpringApplication.run(EmpleosApplication, args)
